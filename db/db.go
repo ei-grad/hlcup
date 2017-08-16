9434213f6a269276f1b1fb3dd96da2359b4e4456
Andrew Grigorev <andrew@ei-grad.ru>
2017-08-17 01:01:19 +0300

    Fuck the hashes

--- db/db.go
+++ db/db.go
@@ -2,30 +2,89 @@ package db
 
 import (
 	sf "github.com/golang/groupcache/singleflight"
+	"sync"
 
 	"github.com/ei-grad/hlcup/models"
 )
 
+type Users []models.User
+
+func (s Users) Get(id uint32) models.User {
+	return s[id]
+}
+
+func (s Users) Set(id uint32, v models.User) {
+	s[id] = v
+}
+
+type Locations []models.Location
+
+func (s Locations) Get(id uint32) models.Location {
+	return s[id]
+}
+
+func (s Locations) Set(id uint32, v models.Location) {
+	s[id] = v
+}
+
+type Visits []models.Visit
+
+func (s Visits) Get(id uint32) models.Visit {
+	return s[id]
+}
+
+func (s Visits) Set(id uint32, v models.Visit) {
+	s[id] = v
+}
+
+type LocationMarks []*models.LocationMarks
+
+func (s LocationMarks) Get(id uint32) *models.LocationMarks {
+	return s[id]
+}
+
+func (s LocationMarks) Set(id uint32, v *models.LocationMarks) {
+	s[id] = v
+}
+
+type UserVisits []*models.UserVisits
+
+func (s UserVisits) Get(id uint32) *models.UserVisits {
+	return s[id]
+}
+
+func (s UserVisits) Set(id uint32, v *models.UserVisits) {
+	s[id] = v
+}
+
 // DB is inmemory database optimized for its task
 type DB struct {
-	users     *models.UserMap
-	locations *models.LocationMap
-	visits    *models.VisitMap
+	users     Users
+	locations Locations
+	visits    Visits
 
-	locationMarks *models.LocationMarksMap
-	userVisits    *models.UserVisitsMap
+	locationMarks LocationMarks
+	userVisits    UserVisits
 
 	locationSF sf.Group
 	userSF     sf.Group
+
+	mu sync.RWMutex
 }
+
+var (
+	maxUsers     = 500000
+	maxLocations = 500000
+	maxVisits    = 5000000
+)
 
 // New creates new DB
 func New() *DB {
 	return &DB{
-		users:         models.NewUserMap(509),
-		locations:     models.NewLocationMap(509),
-		visits:        models.NewVisitMap(509),
-		locationMarks: models.NewLocationMarksMap(509),
-		userVisits:    models.NewUserVisitsMap(509),
+		users:         make([]models.User, maxUsers),
+		locations:     make([]models.Location, maxLocations),
+		visits:        make([]models.Visit, maxVisits),
+		locationMarks: make([]*models.LocationMarks, maxLocations),
+		userVisits:    make([]*models.UserVisits, maxUsers),
 	}
 }
